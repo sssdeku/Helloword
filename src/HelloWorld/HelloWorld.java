3b72d7649a147d198d30b0698588b6488b612d22
zxj <zxj@192.168.56.1>
2019-07-08 09:24:58 +0800

    test

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -3,6 +3,6 @@ package HelloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Ëæ±ã¸ÄÒ»¸Ä");
+		System.out.println("Hello World éšä¾¿æ”¹ä¸€æ”¹");
 	}
 }
